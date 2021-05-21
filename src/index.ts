be506c76b525b7b178292e42192375130c727874
Yingxiliuguang <61727949+Yingxiliuguang@users.noreply.github.com>
2021-05-21 18:35:54 +0800

    feat: 寻找模块根路径逻辑开发

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { getRootPath } from "./utils";
+import { getRootPath, getRelativePath } from "./utils";
+import * as path from 'path';
 
 export function move(
   source: string,
@@ -11,7 +12,10 @@ export function move(
     };
   }
 ) {
+  source = path.isAbsolute(source) ? source : path.resolve(source);
+  target = path.isAbsolute(target) ? target : path.resolve(target);
   const rootPath = getRootPath(source, target, options?.root);
+  getRelativePath(rootPath, target);
   // 找到 root 下所有使用 source 的地方，建议在另一个文件的中封装。
   // 根据jsconfig、tsconfig，生成target所有可能使用路径——相对路径、alias等，找到最短的一个，建议在另一个文件中封装。
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,56 @@
+  // 找到项目根路径。
+  // 如果有root用root，没有的话用source、target中最外的路径向外找，找到最近的一个package.json所在路径即为root。
+  // 如果找不到则返回空字符串。
+import * as fs from 'fs';
+import * as path from 'path';
 export function getRootPath(
   source: string,
   target: string,
   root?: string
 ) {
-  // 找到项目根路径。
-  // 如果有root用root，没有的话用source、target中最外的路径向外找，找到最近的一个package.json所在路径即为root。
-  // 如果找不到则返回空字符串。
+  if(root) return root;
+  // console.info(source, target);
+  const len: number = Math.min(source.length, target.length);
+  let commonPath: string = '';
+  for(let i = 0; i < len; i++) {
+    if(source[i] !== target[i]) {
+      commonPath = source.substring(0, i);
+      break;
+    }
+  }
+  while(commonPath && !fs.existsSync(path.join(commonPath, './package.json'))) {
+    commonPath = path.join(commonPath, '../');
+  }
+  // console.info('true', commonPath);
+  return commonPath;
+}
+
+export function getRelativePath(
+  rootPath: string,
+  target: string,
+) {
+  console.info(rootPath);
+  const configFileArray = [];
+  if(fs.existsSync(path.join(rootPath, './jsconfig.json'))) {
+    configFileArray.push(path.join(rootPath, './jsconfig.json'));
+  }
+  if(fs.existsSync(path.join(rootPath, './tsconfig.json'))) {
+    configFileArray.push(path.join(rootPath, './tsconfig.json'));
+  }
+  // console.info(configFileArray);
+  configFileArray.forEach(item => {
+    fs.readFile(item, 'utf8', (err, dataString) => {
+      if(err) throw err;
+      // console.info(dataString);
+      const dataObject = JSON.parse(dataString);
+      const pathConfig = dataObject?.compilerOptions?.paths;
+      console.info(pathConfig);
+      // const a = Object.keys(pathConfig);
+      const res = [];
+      Object.keys(pathConfig).forEach(item => {
+        // pathConfig[item] = 
+      })
+      console.info(a);
+    });
+  })
 }
